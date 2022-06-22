86119e02ab5f276006701b4a405f62d7605e291f
chepark <chaeahpark.kr@gmail.com>
2022-06-22 21:16:22 +0300

    Finished the server side of projects.

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -15,7 +15,7 @@ const getProject = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).json({ error: "No such workout." });
+    return res.status(404).json({ error: "Invalid project id." });
 
   const project = await Project.findById(id);
 
@@ -44,4 +44,44 @@ const createProject = async (req, res) => {
   }
 };
 
-export { getProjects, getProject, createProject };
+// update a project
+const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const dataToUpdate = { ...req.body };
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "Invalid project id." });
+
+  const project = await Project.findOneAndUpdate(
+    { _id: id },
+    {
+      ...dataToUpdate,
+    }
+  );
+
+  if (!project) return res.status(400).json({ error: "No project to update." });
+
+  res.status(200).json({ project });
+};
+
+// delete a project
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  // check id is valid.
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ error: "Invalid project id." });
+
+  const project = await Project.findOneAndDelete({ _id: id });
+
+  // If project does not exist,
+  // return error.
+  if (!project) return res.status(400).json({ error: "No project to delete." });
+
+  // send a response
+  // if a project is deleted.
+  res.status(200).json({ project });
+};
+
+export { getProjects, getProject, createProject, updateProject, deleteProject };
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,14 +3,23 @@ import {
   createProject,
   getProjects,
   getProject,
+  updateProject,
+  deleteProject,
 } from "../controllers/projectController.js";
 
 const router = express.Router();
 
+// Get all projects
 router.get("/", getProjects);
 
 router.get("/:id", getProject);
 
+// Create a project
 router.post("/", createProject);
+
+// Edit a project
+router.patch("/:id", updateProject);
+// Delete a project
+router.delete("/:id", deleteProject);
 
 export default router;
