29e79e396ba85e907b66a5b92cff3fb6b4a9ab71
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-08-30 16:00:22 +0300

    changed banner

---
+++ client/src/components/Banner/Banner.component.jsx
@@ -0,0 +1,27 @@
+import "./_banner.scss";
+import useWindowSize from "../../hooks/useWindowSize";
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  const [windowHeight, windowWidth] = useWindowSize();
+
+  return (
+    <div className="container" data-section="banner">
+      <div className="content-wrapper" data-section="banner">
+        <div className="banner-textContent">
+          <div className="banner-text__title">
+            Find Teams and Side Projects{" "}
+          </div>
+          <div className="banner-text__subTitle">
+            Exciting place to level up portfolio with future developers.
+          </div>
+          <Link className="banner-button btn-primary" to="/faq">
+            Learn More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
